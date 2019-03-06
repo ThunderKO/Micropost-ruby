ac50159a3ee7bb1332bc1aee6f652a197168c8a8
ThunderKO <ko.ting.andy@gmail.com>
2019-03-07 01:49:25 +0800

    init commit

---
+++ Gemfile
@@ -0,0 +1,27 @@
+source 'https://rubygems.org' 
+ 
+gem 'rails',                '4.2.10' 
+gem 'bcrypt', git: 'https://github.com/codahale/bcrypt-ruby.git', :require => 'bcrypt'
+gem 'faker',                '1.4.2' 
+gem 'will_paginate',        '3.0.7' 
+gem 'bootstrap-will_paginate', '0.0.10' 
+gem 'bootstrap-sass',       '3.2.0.0' 
+gem 'sprockets',            '3.6.3' 
+gem 'sass-rails',           '5.0.3' 
+gem 'uglifier',             '2.5.3' 
+gem 'coffee-rails',         '4.1.0' 
+gem 'jquery-rails',         '4.0.3' 
+gem 'turbolinks',           '2.5.3' 
+gem 'jbuilder',             '2.2.3' 
+gem 'sdoc',                 '0.4.0', group: :doc 
+ 
+# downgraded for problems on Windows 
+gem 'coffee-script-source', '1.8.0' 
+ 
+gem 'sqlite3',     '1.3.13' 
+gem 'byebug',      '3.4.0' 
+gem 'web-console', '2.0.0' 
+gem 'spring',      '1.1.3' 
+ 
+# Windows does not include zoneinfo files, so bundle the tzinfo-data gem 
+gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
---
+++ app/models/user.rb
@@ -0,0 +1,36 @@
+class User < ActiveRecord::Base
+    has_many :microposts, dependent: :destroy
+    has_many :active_relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+    has_many :passive_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+    has_many :following, through: :active_relationships, source: :followed
+    has_many :followers, through: :passive_relationships, source: :follower
+    before_save { self.email = email.downcase }
+    before_save :create_remember_token
+    validates :name, presence: true, length: { maximum: 50 }
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+    validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
+    validates :password, presence: true, length: { minimum: 6 }
+    validates :password_confirmation, presence: true
+    has_secure_password
+
+    def create_remember_token
+        self.remember_token = SecureRandom.urlsafe_base64
+    end
+
+    def feed
+        following_ids = "SELECT followed_id FROM relationships WHERE follower_id = :user_id"
+        Micropost.where("user_id IN (#{following_ids}) OR user_id = :user_id", user_id: id)
+    end
+
+    def follow(other_user)
+        active_relationships.create(followed_id: other_user.id)
+    end
+
+    def unfollow(other_user)
+        active_relationships.find_by(followed_id: other_user.id).destroy
+    end
+
+    def following?(other_user)
+        following.include?(other_user)
+    end
+end
